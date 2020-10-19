f971616680109ce4d954bd4f6bcf45da274e0181
hannanel gershinsky <hannanel100@gmail.com>
2020-10-19 21:42:02 +0300

    img fix

--- src/mock/data.js
+++ src/mock/data.js
@@ -31,7 +31,7 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'feeding-times.JPG',
+    img: 'feeding-times.PNG',
     title: 'Feeding-Times',
     info: "This App is for new mothers to record the new baby's feeding times.",
     info2: 'Using Reactjs and firebase for authentication and database',
